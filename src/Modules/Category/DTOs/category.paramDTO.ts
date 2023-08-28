bdc2dd6b35814f1396ae068273cb43b51c6a2d2e
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 08:52:13 -0300

    feat: impl -> CategoryParamDTO

--- src/Modules/Category/DTOs/category.paramDTO.ts
+++ src/Modules/Category/DTOs/category.paramDTO.ts
@@ -1,1 +1,11 @@
-export class CategoryParamDTO {}
+import { IsOptional, IsString } from 'class-validator';
+
+export class CategoryParamDTO {
+    @IsString()
+    @IsOptional()
+    token?: string;
+
+    @IsString()
+    @IsOptional()
+    name?: string;
+}

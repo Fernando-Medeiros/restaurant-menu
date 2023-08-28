579dfbb2cd6bddf0653fffb0658324d5fd4878db
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2023-08-28 01:22:13 -0300

    Feat[exception]: Erros, Interceptors (#2)
    * feat[tsconfig]: paths (alias)
    
    * feat[exceptions]: BadRequest, NotFound [Erros, Interceptors]
    
    * feat[build]: Global Interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestInterceptor } from 'interceptors/badrequest.interceptor';
+import { NotFoundInterceptor } from 'interceptors/notfound.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.useGlobalInterceptors(
+        ...[new NotFoundInterceptor(), new BadRequestInterceptor()],
+    );
+
     await app.listen(3000);
 }
 bootstrap();

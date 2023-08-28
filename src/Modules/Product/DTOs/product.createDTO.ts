a6ded9d5cad65523d42cd3f244c443a1f02aba9f
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 09:34:30 -0300

    feat: impl -> ProductCreateDTO

--- src/Modules/Product/DTOs/product.createDTO.ts
+++ src/Modules/Product/DTOs/product.createDTO.ts
@@ -1,1 +1,31 @@
-export class ProductCreateDTO {}
+import {
+    IsNotEmpty,
+    IsNumber,
+    IsString,
+    Length,
+    Max,
+    Min,
+} from 'class-validator';
+
+export class ProductCreateDTO {
+    @IsString()
+    @IsNotEmpty()
+    @Length(3, 100)
+    name: string;
+
+    @IsNumber({ maxDecimalPlaces: 2 })
+    @IsNotEmpty()
+    @Min(1)
+    @Max(999)
+    price: number;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(3, 255)
+    imageUrl: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Length(3, 255)
+    description: string;
+}

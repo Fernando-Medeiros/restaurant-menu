4932d26502e13d116a3c50fdf3b2783ef1411625
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 08:52:38 -0300

    feat: impl -> CategoryUpdateDTO

--- src/Modules/Category/DTOs/category.updateDTO.ts
+++ src/Modules/Category/DTOs/category.updateDTO.ts
@@ -1,1 +1,4 @@
-export class CategoryUpdateDTO {}
+import { PartialType } from '@nestjs/mapped-types';
+import { CategoryCreateDTO } from './category.createDTO';
+
+export class CategoryUpdateDTO extends PartialType(CategoryCreateDTO) {}

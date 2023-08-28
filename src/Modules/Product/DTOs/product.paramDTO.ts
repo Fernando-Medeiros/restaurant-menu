09c14e26c49eb00ff222af3ea93165feb3407c25
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 15:06:54 -0300

    feat[modules]: ParamDTO -> add ApiProperty

--- src/Modules/Product/DTOs/product.paramDTO.ts
+++ src/Modules/Product/DTOs/product.paramDTO.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Length, IsOptional } from 'class-validator';
 
 export class ProductParamDTO {
+    @ApiProperty({ description: 'Token' })
     @IsString()
     @IsOptional()
     token?: string;
 
+    @ApiProperty({ description: 'Name' })
     @IsString()
     @IsOptional()
     @Length(3, 100)

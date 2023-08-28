39fe9cbfe361d04f7da820da2246c99970d4cc7f
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 09:34:51 -0300

    feat: impl -> ProductParamDTO

--- src/Modules/Product/DTOs/product.paramDTO.ts
+++ src/Modules/Product/DTOs/product.paramDTO.ts
@@ -1,1 +1,12 @@
-export class ProductParamDTO {}
+import { IsString, Length, IsOptional } from 'class-validator';
+
+export class ProductParamDTO {
+    @IsString()
+    @IsOptional()
+    token?: string;
+
+    @IsString()
+    @IsOptional()
+    @Length(3, 100)
+    name?: string;
+}

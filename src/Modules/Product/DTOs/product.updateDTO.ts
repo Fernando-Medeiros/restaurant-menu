fd4f2d83dff84337a3a4530712d77a6f5ca0dd05
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 09:35:23 -0300

    feat: impl -> ProductUpdateDTO

--- src/Modules/Product/DTOs/product.updateDTO.ts
+++ src/Modules/Product/DTOs/product.updateDTO.ts
@@ -1,1 +1,4 @@
-export class ProductUpdateDTO {}
+import { PartialType } from '@nestjs/mapped-types';
+import { ProductCreateDTO } from './product.createDTO';
+
+export class ProductUpdateDTO extends PartialType(ProductCreateDTO) {}

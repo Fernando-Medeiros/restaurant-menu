e4b9d61580c2dda99383ed94ed218ea8b728760e
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 09:03:44 -0300

    feat: impl -> CategoryResource

--- src/Modules/Category/Resources/category.resource.ts
+++ src/Modules/Category/Resources/category.resource.ts
@@ -1,1 +1,17 @@
-export class CategoryResource {}
+import { CategoryDTO } from '../DTOs/categoryDTO';
+
+export class CategoryResource {
+    token: string;
+    name: string;
+    createdAt: Date;
+
+    constructor(dto: CategoryDTO) {
+        this.token = dto.token;
+        this.name = dto.name;
+        this.createdAt = dto.createdAt;
+    }
+
+    static toArray(arr: CategoryDTO[]): CategoryResource[] {
+        return arr.length > 0 ? arr.filter((c) => new CategoryResource(c)) : [];
+    }
+}

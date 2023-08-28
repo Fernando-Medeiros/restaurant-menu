863a4401253780c0d1eef71257bb42653dc6be75
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-28 15:07:26 -0300

    feat[modules]: CreateDTO -> add ApiProperty

--- src/Modules/Product/DTOs/product.createDTO.ts
+++ src/Modules/Product/DTOs/product.createDTO.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
     IsNotEmpty,
     IsNumber,
@@ -8,22 +9,26 @@ import {
 } from 'class-validator';
 
 export class ProductCreateDTO {
+    @ApiProperty({ description: 'Product Name' })
     @IsString()
     @IsNotEmpty()
     @Length(3, 100)
     name: string;
 
+    @ApiProperty({ description: 'Price' })
     @IsNumber({ maxDecimalPlaces: 2 })
     @IsNotEmpty()
     @Min(1)
     @Max(999)
     price: number;
 
+    @ApiProperty({ description: 'Image URL' })
     @IsString()
     @IsNotEmpty()
     @Length(3, 255)
     imageUrl: string;
 
+    @ApiProperty({ description: 'Description' })
     @IsString()
     @IsNotEmpty()
     @Length(3, 255)

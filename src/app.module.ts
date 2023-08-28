c4cabdfd47a31e9162ceea1d23f4b844525fc27c
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-27 22:33:52 -0300

    feat[module]: App -> import [ProductModule, CategoryModule]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
+import { ProductModule } from './Modules/Product/product.module';
+import { CategoryModule } from './Modules/Category/category.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [],
+    imports: [ProductModule, CategoryModule],
     controllers: [],
     providers: [],
 })

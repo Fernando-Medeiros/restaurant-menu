a9ae247eff24190fdd50a4adb7255396bab37ee3
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-29 01:47:41 -0300

    feat: Impl ProductRepository + Unit Tests

--- src/Modules/Product/Repository/product.repository.ts
+++ src/Modules/Product/Repository/product.repository.ts
@@ -1,29 +1,50 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ProductQueryDTO } from '../DTOs/product.queryDTO';
 import { ProductParamDTO } from '../DTOs/product.paramDTO';
 import { ProductCreateDTO } from '../DTOs/product.createDTO';
 import { ProductUpdateDTO } from '../DTOs/product.updateDTO';
 import { ProductDTO } from '../DTOs/productDTO';
+import { PrismaService } from 'modulesHelpers/Prisma/prisma.service';
 
 @Injectable()
 export class ProductRepository {
-    async findMany(dto: ProductQueryDTO): Promise<ProductDTO[] | []> {
-        throw new NotImplementedException('');
+    constructor(private readonly _context: PrismaService) {}
+
+    async findMany(query: ProductQueryDTO): Promise<ProductDTO[] | []> {
+        const { order, sort } = query;
+
+        const orderBy = {
+            token: { token: order },
+            name: { name: order },
+            createdAt: { createdAt: order },
+        };
+
+        return this._context.product.findMany({
+            take: +query.take,
+            skip: +query.skip,
+            orderBy: orderBy[sort],
+        });
     }
 
     async findOne(dto: ProductParamDTO): Promise<ProductDTO | null> {
-        throw new NotImplementedException('');
+        const { token, name } = dto;
+
+        const query = token ? { token } : name ? { name } : null;
+
+        if (query == null) return null;
+
+        return this._context.product.findFirst({ where: query });
     }
 
     async register(dto: ProductCreateDTO): Promise<void> {
-        throw new NotImplementedException('');
+        await this._context.product.create({ data: dto });
     }
 
-    async update(dto: ProductUpdateDTO): Promise<void> {
-        throw new NotImplementedException('');
+    async update(token: string, dto: ProductUpdateDTO): Promise<void> {
+        await this._context.product.update({ where: { token }, data: dto });
     }
 
-    async remove(dto: ProductDTO): Promise<void> {
-        throw new NotImplementedException('');
+    async remove(token: string): Promise<void> {
+        await this._context.product.delete({ where: { token } });
     }
 }

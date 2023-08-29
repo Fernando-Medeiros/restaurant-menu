b315d2989baa25a12f4b4805bff49dd4f9b05692
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-29 00:42:30 -0300

    refactor: CategoryUpdateDTO

--- src/Modules/Category/DTOs/category.updateDTO.ts
+++ src/Modules/Category/DTOs/category.updateDTO.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CategoryCreateDTO } from './category.createDTO';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty, Length } from 'class-validator';
 
-export class CategoryUpdateDTO extends PartialType(CategoryCreateDTO) {}
+export class CategoryUpdateDTO {
+    @ApiProperty({ description: 'Name' })
+    @IsString()
+    @IsNotEmpty()
+    @Length(3, 100)
+    name: string;
+}

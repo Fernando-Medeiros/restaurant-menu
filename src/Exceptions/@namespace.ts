106586d139787cce6d0be67df10c4ed1130c46e1
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-29 15:55:02 -0300

    feat: namespaces, [export * from current module]

---
+++ src/Exceptions/@namespace.ts
@@ -0,0 +1,5 @@
+export * from './types/badrequest.error';
+export * from './types/notfound.error';
+export * from './interceptors/base.Interceptor';
+export * from './interceptors/badrequest.interceptor';
+export * from './interceptors/notfound.interceptor';

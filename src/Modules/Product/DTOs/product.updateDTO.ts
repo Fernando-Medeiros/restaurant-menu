b040f4e5d58e9fa01b9bd427af7cab962c516011
Fernando Medeiros <97191840+Fernando-Medeiros@users.noreply.github.com>
2023-08-29 03:01:17 -0300

    Feat[module]: Impl ProductController + Unit Tests (#12)
    * fix: ProductParamDTO -> ApiPropertyOptional
    
    * fix: ProductQueryDTO -> skip default(0)
    
    * refactor: ProductUpdateDTO
    
    * feat[module]: Impl ProductController + Unit Tests

--- src/Modules/Product/DTOs/product.updateDTO.ts
+++ src/Modules/Product/DTOs/product.updateDTO.ts
@@ -1,4 +1,36 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ProductCreateDTO } from './product.createDTO';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+    IsString,
+    Length,
+    IsNumber,
+    Min,
+    Max,
+    IsOptional,
+} from 'class-validator';
 
-export class ProductUpdateDTO extends PartialType(ProductCreateDTO) {}
+export class ProductUpdateDTO {
+    @ApiPropertyOptional({ description: 'Product Name' })
+    @IsOptional()
+    @IsString()
+    @Length(3, 100)
+    name: string;
+
+    @ApiPropertyOptional({ description: 'Price' })
+    @IsOptional()
+    @IsNumber({ maxDecimalPlaces: 2 })
+    @Min(1)
+    @Max(999)
+    price: number;
+
+    @ApiPropertyOptional({ description: 'Image URL' })
+    @IsOptional()
+    @IsString()
+    @Length(3, 255)
+    imageUrl: string;
+
+    @ApiPropertyOptional({ description: 'Description' })
+    @IsOptional()
+    @IsString()
+    @Length(3, 255)
+    description: string;
+}

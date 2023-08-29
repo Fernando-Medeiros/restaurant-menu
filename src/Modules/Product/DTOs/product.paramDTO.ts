c55da481a22c0cacebb48dbe998297731551443c
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-29 02:57:41 -0300

    fix: ProductParamDTO -> ApiPropertyOptional

--- src/Modules/Product/DTOs/product.paramDTO.ts
+++ src/Modules/Product/DTOs/product.paramDTO.ts
@@ -1,14 +1,14 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsString, Length, IsOptional } from 'class-validator';
 
 export class ProductParamDTO {
-    @ApiProperty({ description: 'Token' })
+    @ApiPropertyOptional({ description: 'Token' })
     @IsString()
     @IsOptional()
     @Length(24, 24)
     token?: string;
 
-    @ApiProperty({ description: 'Name' })
+    @ApiPropertyOptional({ description: 'Name' })
     @IsString()
     @IsOptional()
     @Length(3, 100)

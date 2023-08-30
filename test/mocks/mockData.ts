a3988dcf32a45a454c504d40bca1e9543019cfd5
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-30 17:58:00 -0300

    feat[tests]: MockMenu, MockMenuResource

--- test/mocks/mockData.ts
+++ test/mocks/mockData.ts
@@ -1,8 +1,9 @@
 import { CategoryDTO, CategoryResource } from 'modules/Category/@namespace';
+import { MenuDTO, MenuResource } from 'modules/Menu/@namespace';
 import { ProductDTO, ProductResource } from 'modules/Product/@namespace';
 
 export const MockCategory: CategoryDTO = {
-    token: '64ed4ffa6d30117c1189f262',
+    token: '64ed4ffa6d30188c1189f262',
     name: 'Mock Category',
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -22,6 +23,15 @@ export const MockProduct: ProductDTO = {
     categories: [] as CategoryDTO[],
 };
 
+export const MockMenu: MenuDTO = {
+    token: '64ed4ffa6d30117c1184fd62',
+    productToken: MockProduct.token,
+    period: 'daytime',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    product: MockProduct,
+};
+
 export const MockCategoryResource: CategoryResource = new CategoryResource(
     MockCategory,
 );
@@ -29,3 +39,5 @@ export const MockCategoryResource: CategoryResource = new CategoryResource(
 export const MockProductResource: ProductResource = new ProductResource(
     MockProduct,
 );
+
+export const MockMenuResource: MenuResource = new MenuResource(MockMenu);

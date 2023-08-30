4d55b9c1eb47b37b95b44edc80a057259124a04e
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-30 14:23:26 -0300

    feat[module][resource]: MenuResource

---
+++ src/Modules/Menu/Resources/menu.resource.ts
@@ -0,0 +1,29 @@
+import { Period } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { MenuDTO } from 'modules/Menu/@namespace';
+import { ProductResource } from 'modules/Product/@namespace';
+
+export class MenuResource {
+    @ApiProperty()
+    token: string;
+    @ApiProperty()
+    productToken: string;
+    @ApiProperty({ enum: Period })
+    period: Period;
+    @ApiProperty()
+    createdAt: Date;
+    @ApiProperty({ type: ProductResource })
+    product: ProductResource;
+
+    constructor(dto: MenuDTO) {
+        this.token = dto.token;
+        this.productToken = dto.productToken;
+        this.period = dto.period;
+        this.createdAt = dto.createdAt;
+        this.product = new ProductResource(dto.product);
+    }
+
+    static toArray(arr: MenuDTO[]): MenuResource[] {
+        return arr?.length > 0 ? arr.map((c) => new MenuResource(c)) : [];
+    }
+}

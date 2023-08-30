a0689a10bfff431dbf08311074905f8df1e4185a
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-30 02:25:05 -0300

    feat: CategoryResourceToProducts

--- src/Modules/Product/Resources/product.resource.ts
+++ src/Modules/Product/Resources/product.resource.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { CategoryResource } from 'modules/Category/@namespace';
+import { CategoryResourceToProducts } from 'modules/Category/@namespace';
 import { ProductDTO } from 'modules/Product/@namespace';
 
 export class ProductResource {
@@ -18,8 +18,8 @@ export class ProductResource {
     @ApiProperty()
     description: string;
 
-    @ApiProperty()
-    categories: CategoryResource[];
+    @ApiProperty({ isArray: true, type: CategoryResourceToProducts })
+    categories: CategoryResourceToProducts[];
 
     @ApiProperty()
     createdAt: Date;
@@ -31,7 +31,7 @@ export class ProductResource {
         this.imageUrl = dto.imageUrl;
         this.description = dto.description;
         this.createdAt = dto.createdAt;
-        this.categories = CategoryResource.toArray(dto.categories);
+        this.categories = CategoryResourceToProducts.toArray(dto.categories);
     }
 
     static toArray(arr: ProductDTO[]): ProductResource[] {

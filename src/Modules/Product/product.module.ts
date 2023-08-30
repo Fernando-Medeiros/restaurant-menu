4c45ce2d2e03ad5c32d0e3d77c0fb9c7bc65277a
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-08-29 23:32:03 -0300

    feat[module][product]: providers [CategoryService, CategoryRepository]

--- src/Modules/Product/product.module.ts
+++ src/Modules/Product/product.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'modulesHelpers/Prisma/prisma.service';
 import { ProductController } from 'controllers/v1/@namespace';
 import { ProductRepository, ProductService } from 'modules/Product/@namespace';
+import {
+    CategoryRepository,
+    CategoryService,
+} from 'modules/Category/@namespace';
 
 @Module({
     imports: [],
     controllers: [ProductController],
-    providers: [PrismaService, ProductService, ProductRepository],
+    providers: [
+        PrismaService,
+        ProductService,
+        ProductRepository,
+        CategoryService,
+        CategoryRepository,
+    ],
 })
 export class ProductModule {}

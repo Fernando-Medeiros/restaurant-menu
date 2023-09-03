d8f7be70e1d32421630738aeb34eae13395050bb
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-02 23:06:50 -0300

    refactor[tests]: MockDatas

--- test/mocks/mockData.ts
+++ test/mocks/mockData.ts
@@ -1,16 +1,15 @@
 import { CategoryDTO, CategoryResource } from 'modules/Category/@namespace';
-import { MenuDTO, MenuResource } from 'modules/Menu/@namespace';
 import { ProductDTO, ProductResource } from 'modules/Product/@namespace';
+import { MenuDTO, MenuResource } from 'modules/Menu/@namespace';
 
 export const MockCategory: CategoryDTO = {
     token: '64ed4ffa6d30188c1189f262',
     name: 'Mock Category',
     createdAt: new Date(),
     updatedAt: new Date(),
-    productsIDs: [] as string[],
-    products: [] as ProductDTO[],
+    productsIDs: [],
+    products: [],
 };
-
 export const MockProduct: ProductDTO = {
     token: '64ed4ffa6d30117c1189f262',
     name: 'Mock Product',
@@ -19,10 +18,9 @@ export const MockProduct: ProductDTO = {
     price: 9.99,
     createdAt: new Date(),
     updatedAt: new Date(),
-    categoriesIDs: [] as string[],
-    categories: [] as CategoryDTO[],
+    categoriesIDs: [],
+    categories: [],
 };
-
 export const MockMenu: MenuDTO = {
     token: '64ed4ffa6d30117c1184fd62',
     productToken: MockProduct.token,
@@ -32,12 +30,6 @@ export const MockMenu: MenuDTO = {
     product: MockProduct,
 };
 
-export const MockCategoryResource: CategoryResource = new CategoryResource(
-    MockCategory,
-);
-
-export const MockProductResource: ProductResource = new ProductResource(
-    MockProduct,
-);
-
-export const MockMenuResource: MenuResource = new MenuResource(MockMenu);
+export const MockCategoryResource = new CategoryResource(MockCategory);
+export const MockProductResource = new ProductResource(MockProduct);
+export const MockMenuResource = new MenuResource(MockMenu);

9b8dda768a0d8dfd4cbfdc8706ba8e4c22aa5cb2
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-02 22:55:22 -0300

    refactor[module][product]: ProductRepository (queries)

--- src/Modules/Product/Repository/product.repository.ts
+++ src/Modules/Product/Repository/product.repository.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'modulesHelpers/Prisma/prisma.service';
 import {
-    ProductCreateDTO,
     ProductDTO,
-    ProductParamDTO,
     ProductQueryDTO,
+    ProductParamDTO,
+    ProductCreateDTO,
     ProductUpdateDTO,
 } from 'modules/Product/@namespace';
 
@@ -12,34 +12,25 @@ import {
 export class ProductRepository {
     constructor(private readonly _context: PrismaService) {}
 
-    async findMany(query: ProductQueryDTO): Promise<ProductDTO[] | []> {
-        const { order, sort } = query;
-
-        const orderBy = {
-            token: { token: order },
-            name: { name: order },
-            price: { price: order },
-            createdAt: { createdAt: order },
-        };
-
-        return this._context.product.findMany({
-            take: Math.abs(+query.take),
-            skip: Math.abs(+query.skip),
-            orderBy: orderBy[sort],
-            include: { categories: true },
-        });
+    async findMany(
+        query: ProductQueryDTO,
+    ): Promise<{ total: number; data: ProductDTO[] }> {
+        const [total, data] = await Promise.all([
+            this._context.product.count(),
+            this._context.product.findMany({
+                ...query.extractResolvedFilters(),
+                include: { categories: true },
+            }),
+        ]);
+        return { total, data };
     }
 
     async findOne(dto: ProductParamDTO): Promise<ProductDTO | null> {
         const { token, name } = dto;
 
-        const query = token ? { token } : name ? { name } : null;
-
-        if (query == null) return null;
-
         return this._context.product.findFirst({
-            where: query,
             include: { categories: true },
+            where: { OR: [token && { token }, name && { name }] },
         });
     }
 

e3bbb8253c1732e151a6e0d43dd7718c4fe4fbdd
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-02 22:59:02 -0300

    feat[module][category]: CategoryQueryDTO

--- src/Modules/Category/DTOs/category.queryDTO.ts
+++ src/Modules/Category/DTOs/category.queryDTO.ts
@@ -1,42 +1,14 @@
-import { IsEnum, IsNumberString, Length } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { OrderBy } from '../Enum/orderby';
-import { SortBy } from '../Enum/sortby';
+import { IsEnum } from 'class-validator';
+import { ESortBy } from '../Enums/sortby.enum';
+import { QueryDTO } from 'modulesHelpers/Common/@namespace';
 
-export class CategoryQueryDTO {
-    @ApiPropertyOptional({
-        description: 'Take',
-        default: 10,
-        minimum: 1,
-        maximum: 99,
-    })
-    @Length(0, 2, { message: 'must be greater than 1 and less than 99' })
-    @IsNumberString()
-    take?: string = '10';
-
-    @ApiPropertyOptional({
-        description: 'Skip',
-        default: 0,
-        minimum: 0,
-        maximum: 99,
-    })
-    @Length(0, 2, { message: 'must be greater than 0 and less than 99' })
-    @IsNumberString()
-    skip?: string = '0';
-
-    @ApiPropertyOptional({
-        description: 'Order By',
-        default: 'asc',
-        enum: OrderBy,
-    })
-    @IsEnum(OrderBy)
-    order?: OrderBy = OrderBy.asc;
-
+export class CategoryQueryDTO extends QueryDTO {
     @ApiPropertyOptional({
         description: 'Sort By Columns',
         default: 'name',
-        enum: SortBy,
+        enum: ESortBy,
     })
-    @IsEnum(SortBy)
-    sort?: SortBy = SortBy.name;
+    @IsEnum(ESortBy)
+    public sort?: ESortBy = ESortBy.name;
 }

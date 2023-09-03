acf35980794a608d5128ea652495f466c2cc2419
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-09-02 23:03:25 -0300

    feat[module][menu]: MenuQueryDTO

--- src/Modules/Menu/DTOs/menu.queryDTO.ts
+++ src/Modules/Menu/DTOs/menu.queryDTO.ts
@@ -1,46 +1,14 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { Length, IsEnum, IsNumberString } from 'class-validator';
-import { OrderBy } from '../Enum/orderby';
-import { SortBy } from '../Enum/sortby';
+import { IsEnum } from 'class-validator';
+import { ESortBy } from '../Enums/sortby.enum';
+import { QueryDTO } from 'modulesHelpers/Common/@namespace';
 
-export class MenuQueryDTO {
-    @ApiPropertyOptional({
-        description: 'Take',
-        default: 10,
-        minimum: 1,
-        maximum: 99,
-        type: 'string',
-    })
-    @Length(0, 2, { message: 'must be greater than 1 and less than 99' })
-    @IsNumberString()
-    take?: string = '10';
-
-    @ApiPropertyOptional({
-        description: 'Skip',
-        default: 0,
-        minimum: 0,
-        maximum: 99,
-        type: 'string',
-    })
-    @Length(0, 2, {
-        message: 'must be greater or equal than 0 and less than 99',
-    })
-    @IsNumberString()
-    skip?: string = '0';
-
-    @ApiPropertyOptional({
-        description: 'Order By',
-        default: 'asc',
-        enum: OrderBy,
-    })
-    @IsEnum(OrderBy)
-    order?: OrderBy = OrderBy.asc;
-
+export class MenuQueryDTO extends QueryDTO {
     @ApiPropertyOptional({
         description: 'Sort By Columns',
         default: 'createdAt',
-        enum: SortBy,
+        enum: ESortBy,
     })
-    @IsEnum(SortBy)
-    sort?: SortBy = SortBy.createdAt;
+    @IsEnum(ESortBy)
+    sort?: ESortBy = ESortBy.createdAt;
 }
